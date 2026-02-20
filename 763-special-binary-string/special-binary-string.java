8d25dad5b7516efb02add074cd849df5d9fbbdc5
Nikhil dangi <nikhildangi1205@gmail.com>
2026-02-20 11:43:05 +0530

    Time: 2 ms (100.00%) | Memory: 43.1 MB (79.62%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String makeLargestSpecial(String s) {
+        List<String> list = new ArrayList<>();
+        int count = 0;
+        int start = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '1'){
+                count++;
+            }else{
+                count--;
+            }
+            if(count == 0){
+                String inner = makeLargestSpecial(s.substring(start+1, i));
+                list.add("1"+inner+"0");
+                start = i+1;
+            }
+        }
+
+        Collections.sort(list, Collections.reverseOrder());
+        StringBuilder res = new StringBuilder();
+
+        for(String st:list){
+            res.append(st);
+        }
+
+        return res.toString();
+    }
+}
