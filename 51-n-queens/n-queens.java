83758b1284616b305c86b7b140384770f8acc57e
Nikhil dangi <nikhildangi1205@gmail.com>
2026-09-08 19:09:10 +0530

    Time: 15 ms (5.52%) | Memory: 52.1 MB (5.07%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,49 +1,52 @@
 class Solution {
-    public static boolean isSafe(char[][] arr,int row ,int j){
+
+    static boolean isvalid(List<String> temp, int row,int col,int n){
         for(int i=row-1;i>=0;i--){
-            if(arr[i][j]=='Q') return false;
-        }
-        for(int i=row-1,k=j-1;i>=0 && k>=0;i--,k--){
-            if(arr[i][k]=='Q') return false;
-        }
-        for(int i=row-1,k=j+1;i>=0 && k<arr.length;i--,k++){
-            if(arr[i][k]=='Q') return false;
-        }
-        return true;
-    }
-    public static void  permutation(char[][] arr,List<List<String>> st,int row){
-        if(row==arr.length){
-           List<String> subst= new ArrayList<>();
-           for(int i=0;i<arr.length;i++){
-            String temp="";
-            for(int j=0;j<arr.length;j++){
-                temp+=arr[i][j];
-            }
-            subst.add(temp);
-            temp="";
-           }
-           st.add(subst);
-           return;
+            if(temp.get(i).charAt(col)=='Q') return false;
         }
 
-        for(int j=0;j<arr.length;j++){
-            if(isSafe(arr,row,j)){
-                arr[row][j]='Q';
-                permutation(arr,st,row+1);
-                arr[row][j]='.';
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if(temp.get(i).charAt(j)=='Q') return false;
+        }
+
+        for(int i=row-1,j=col+1; i>=0 && j<n; i--,j++){
+            if(temp.get(i).charAt(j)=='Q') return false;
+        }
+
+        return true;
+    }
+    static void nQueens(List<List<String>> arr,List<String> temp, int row,int n){
+        if(row==n){
+            arr.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for(int j=0;j<n;j++){
+            if(isvalid(temp,row,j,n)){
+                String str=temp.get(row);
+                str=str.substring(0,j)+'Q'+str.substring(j+1);
+                temp.set(row,str);
+                nQueens(arr,temp,row+1,n);
+                str=temp.get(row);
+                str=str.substring(0,j)+'.'+str.substring(j+1);
+                temp.set(row,str);
             }
         }
     }
+
     public List<List<String>> solveNQueens(int n) {
-        char[][] arr = new char[n][n]; 
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                arr[i][j]='.';
-            }
+        List<List<String>> arr = new ArrayList<>();
+        List<String> temp = new ArrayList<>();
+        if(n==1) {
+            temp.add("Q");
+            arr.add(temp);
+            return arr;
         }
-        List<List<String>> st= new ArrayList();
-        permutation(arr,st,0);
-        return st;
+        for(int i = 0; i < n; i++){
+            temp.add(".".repeat(n));
+        }
         
+        nQueens(arr,temp,0,n);
+        return arr;
     }
 }
